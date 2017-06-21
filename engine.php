2d9c6b4a89ed32791621eaba01a259792935ffcb
BRASSRING\Lee <lee@allisonagile.com>
2017-06-21 17:57:47 -0500

    no message

--- engine.php
+++ engine.php
@@ -1,4 +1,44 @@
 <?php
 
+//Create connection
+$servername = "localhost";
+$username = "lazysumo_ptoseng";
+$password = "soft1sumo";
+//$dbname = "lazysumo_PToSCards";
+
+// Create MySQL connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo "Connected successfully";
+
+
+
+function Get_Card($cell_ID)
+  {
+    global $conn;  
+    //Get the data
+    //$sql = 'SELECT * FROM Cards WHERE Cell = "' . $cell_ID . '"';
+    $sql = 'SELECT * FROM Cards WHERE Cell = "' . $cell_ID . '"';
+    echo $sql;
+    $result = $conn->query($sql);
+    
+    // Need to get this part working, it's now pulling data, just not displaying...
+    
+    
+    //$row = mysql_fetch_array($result);
+    
+    echo $result[Name];;
+  }
+  
+
+
+
+//SELECT * FROM Customers
+//WHERE Country='Mexico';
+
 ?>
 
--- index.php
+++ index.php
@@ -1,4 +1,22 @@
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<?php require 'engine.php';?>          
+
 <?php
 
 ?>
-Test
+
+<table class="Card_Table">
+  <tr>
+    <th>PO</th>
+    <th>DT</th> 
+    <th>SM</th>
+  </tr>
+  <tr>
+    <td><?php Get_Card(1) ?></td>
+    <td><?php Get_Card(2) ?></td> 
+    <td><?php Get_Card(3) ?></td>
+  </tr>
+</table>
